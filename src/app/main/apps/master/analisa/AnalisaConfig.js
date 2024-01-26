7f84cfe01184dd709251398a091f33bac1061b40
muhamad maulana <maulanamuhamad593@gmail.com>
2024-01-26 21:36:41 +0700

    fix

---
+++ src/app/main/apps/master/analisa/AnalisaConfig.js
@@ -0,0 +1,16 @@
+
+import AnalisaConfig from './pages/AnalisaConfig';
+
+const AnalisaConfigConfig = {
+  settings: {
+    layout: {},
+  },
+  routes: [
+    {
+      path: '/apps/AnalisaConfig/',
+      element: <AnalisaConfig />,
+    },
+  ],
+};
+
+export default AnalisaConfigConfig;
